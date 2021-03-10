ec8f96c30af9a1435bf95a02df5850302be4d330
Thokozile Snono <thokozilesinono>
2021-03-10 03:13:38 +0200

    Third commit Remove files and folder(Specific)

--- src/za/ac/cput/adp3git/Thokozile.java
+++ src/za/ac/cput/adp3git/Thokozile.java
@@ -8,7 +8,7 @@ package za.ac.cput.adp3git;
 
 public class Thokozile {
 
-    public int lengthOfString(String str){
+    public int lengthOfString(String str) {
         //The class/file will turn blue now because of the changes that are made
         if (str == null || str.isEmpty()) return 0;
         return str.length();
