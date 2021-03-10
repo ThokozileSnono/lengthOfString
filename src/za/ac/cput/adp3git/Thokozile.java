d9aee9c4441c30bdf84f204b6cb63e2f751f35ff
Thokozile Snono <thokozilesinono>
2021-03-10 02:14:14 +0200

    Second commit *Program modified and functionality of the  lengthOfString() was extended

--- src/za/ac/cput/adp3git/Thokozile.java
+++ src/za/ac/cput/adp3git/Thokozile.java
@@ -1,8 +1,16 @@
 package za.ac.cput.adp3git;
 
+/**
+ * This program returns the length of a string
+ * Name and Surname: Thokozile Snono
+ * Student number: 216032121
+ */
+
 public class Thokozile {
 
     public int lengthOfString(String str){
+        //The class/file will turn blue now because of the changes that are made
+        if (str == null || str.isEmpty()) return 0;
         return str.length();
     }
 }
